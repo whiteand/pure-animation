6f84d8e4da87d65d4f1db3aa08955d20a2a88699
whiteand <andrewbeletskiy@gmail.com>
2021-07-17 00:22:29 +0300

    removed delay function

--- src/delay.ts
+++
@@ -1,8 +0,0 @@
-import { Animation } from "./types";
-
-export function delay<R>(
-  timeDelay: number,
-  animation: Animation<R>
-): Animation<R> {
-  return (time: number) => animation(time - timeDelay);
-}
